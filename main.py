64166632ee2927559db86d93d25ff869c5818520
Calder White <calderwhite1@gmail.com>
2019-11-29 21:08:28 -0500

    Fixed some issues.

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import symtable
 import sys
 
 import astunparse
-import astpretty
 
 builtins = ["abs", "delattr", "hash", "memoryview", "set", "all", "dict", "help",
             "min", "setattr", "any", "dir", "hex", "next", "slice", "ascii", "divmod",
@@ -15,8 +14,9 @@ builtins = ["abs", "delattr", "hash", "memoryview", "set", "all", "dict", "help"
             "locals", "repr", "zip", "compile", "globals", "map", "reversed", "__import__",
             "complex", "hasattr", "max", "round"]
 
+
 class SymbolSwapper(ast.NodeTransformer):
-    
+
     def __init__(self, original, imported, excludes=[]):
         self.imported = imported
         self.original = original
@@ -41,63 +41,54 @@ class SymbolSwapper(ast.NodeTransformer):
 
         return "".join(sym)
 
+    def get_new_sym(self, sym):
+        if sym not in self.excludes:
+            if sym not in self.new_sym_table:
+                self.new_sym_table[sym] = self.next_sym()
+
+            return self.new_sym_table[sym]
+
+        return sym
+
     def visit_Name(self, node: ast.Name):
-        _id = node.id
-        if _id not in self.excludes:
-            if _id not in self.new_sym_table:
-                self.new_sym_table[_id] = self.next_sym()
-
-            node.id = self.new_sym_table[_id]
-
+        node.id = self.get_new_sym(node.id)
         return node
 
     def visit_Attribute(self, node: ast.Attribute):
-        attr = node.attr
-
         if type(node.value) == ast.Name:
             _id = node.value.id
             if _id == "self":
-                if attr not in self.excludes:
-                    if attr not in self.new_sym_table:
-                        self.new_sym_table[attr] = self.next_sym()
-
-                    node.attr = self.new_sym_table[attr]
+                node.attr = self.get_new_sym(node.attr)
             elif node.value.id not in self.excludes:
-                if _id not in self.new_sym_table:
-                    self.new_sym_table[_id] = self.next_sym()
-
-                node.value.id = self.new_sym_table[_id]
+                node.value.id = self.get_new_sym(node.value.id)
         elif type(node.value) == ast.Attribute:
             if type(node.value.value) == ast.Name and node.value.value.id == "self":
-                attr = node.value.attr
-                if attr not in self.excludes:
-                    if attr not in self.new_sym_table:
-                        self.new_sym_table[attr] = self.next_sym()
-
-                    node.value.attr = self.new_sym_table[attr]
+                self.visit(node.value)
+        elif type(node.value) == ast.Subscript:
+            self.visit(node.value)
 
         return node
 
     def visit_FunctionDef(self, node: ast.FunctionDef):
         for i in range(len(node.args.args)):
             name = node.args.args[i].arg
-            if name not in self.excludes:
-                if name not in self.new_sym_table:
-                    self.new_sym_table[name] = self.next_sym()
 
-                node.args.args[i].arg = self.new_sym_table[name]
-
-        name = node.name
-        if name not in self.excludes:
-            if name not in self.new_sym_table:
-                self.new_sym_table[name] = self.next_sym()
-
-            node.name = self.new_sym_table[name]
+            node.args.args[i].arg = self.get_new_sym(name)
+        node.name = self.get_new_sym(node.name)
 
         # recursively change the symbols within the function
         for i in node.body:
             self.generic_visit(i)
 
+        return node
+
+    def visit_Lambda(self, node: ast.Lambda):
+        for i in range(len(node.args.args)):
+            name = node.args.args[i].arg
+
+            node.args.args[i].arg = self.get_new_sym(name)
+
+        self.visit(node.body)
         return node
 
     def print_stats(self):
@@ -108,6 +99,7 @@ class SymbolSwapper(ast.NodeTransformer):
 
         print(len(self.new_sym_table.keys()), "symbols used of max", 1 << c)
 
+
 def get_imports(s):
     imported = []
     table = symtable.symtable(s, "string", "exec")
@@ -117,17 +109,19 @@ def get_imports(s):
 
     return imported
 
-r = open(sys.argv[1]).read()
-imported = get_imports(r)
-parsed = ast.parse(r)
 
-#print(astpretty.pprint(parsed))
+def main(args):
+    r = open(args[1]).read()
+    imported = get_imports(r)
+    parsed = ast.parse(r)
 
-swapper = SymbolSwapper("bruh_moment", imported, excludes=["__init__", "self", "Test",
-                        "get_next_move", "get_pos", "grid_width"])
-tree = swapper.visit(parsed)
+    swapper = SymbolSwapper("wave_check", imported, excludes=["__init__", "self", "Test",
+                            "get_next_move", "get_pos", "grid_width"])
+    tree = swapper.visit(parsed)
 
-swapper.print_stats()
+    swapper.print_stats()
+    open("out.py", 'w').write(astunparse.unparse(tree))
 
-#print(astunparse.unparse(tree))
-open("out.py", 'w').write(astunparse.unparse(tree))
+
+if __name__ == "__main__":
+    main(sys.argv)
